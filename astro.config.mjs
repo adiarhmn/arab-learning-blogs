406c667ba34a7450bba36ba3d8edd9353e860596
adiarhmn <adiauliarahmn@gmail.com>
2026-04-09 14:28:47 +0800

    udpate: font, make simple page, and refine some component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+// LOCAL SITE
+const site = 'http://localhost:4323/';
+// const site = 'https://adiarhmn.github.io';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://adiarhmn.github.io',
+    site,
     vite: {
         plugins: [tailwindcss()],
     },
