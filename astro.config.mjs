35efb8df08c82f595b9423dfcb0a855e6458339a
adiarhmn <adiauliarahmn@gmail.com>
2026-04-09 14:32:04 +0800

    update: site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // LOCAL SITE
-const site = 'http://localhost:4323/';
-// const site = 'https://adiarhmn.github.io';
+// const site = 'http://localhost:4323/';
+const site = 'https://adiarhmn.github.io/';
 
 // https://astro.build/config
 export default defineConfig({
